383fe1b92b8f7cc38f130446557044a9c26d0177
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-08-29 17:35:55 +0200

    Disable rescue_froms and set after signin path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,15 @@ class ApplicationController < ActionController::Base
   end
   
   # unless Rails.application.config.consider_all_requests_local
-    rescue_from Exception, with: lambda { |exception| render_error 500, exception }
-    rescue_from ActionController::RoutingError, ActionController::UnknownController, 
-          ::AbstractController::ActionNotFound, ActiveRecord::RecordNotFound, 
-          with: lambda { |exception| render_error 404, exception }
+  #  rescue_from Exception, with: lambda { |exception| render_error 500, exception }
+  #  rescue_from ActionController::RoutingError, ActionController::UnknownController, 
+  #        ::AbstractController::ActionNotFound, ActiveRecord::RecordNotFound, 
+  #        with: lambda { |exception| render_error 404, exception }
   # end
+  
+  def after_sign_in_path_for(user)
+    dashboard_path
+  end
 
   private
   def render_error(status, exception)
